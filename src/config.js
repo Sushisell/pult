0838109c2552fdbaa0bee0a3d85c7d15e48a9ef0
viktoria1barabanova-pixel <viktoria1barabanova@gmail.com>
2026-06-04 09:53:29 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // Чтобы подключить реальные данные, положите JSON с таблицей в data/workbook.json
 // или замените значение ниже на URL вашего Google Apps Script / API, который отдаёт такой же JSON.
-window.PULT_DATA_URL = './data/workbook.json';
+window.PULT_DATA_URL = 'https://script.google.com/macros/s/AKfycbwI53Gvmu2C10uua6zXImGVizVo71dTR3_fnM_0gMIOel-jd1WAbsM0pESxflHxAxlRRA/exec';
